5d461cb5ac40e20eaacae26ccd2b211f8381df4c
jamesnz007 <evans.james.edward@gmail.com>
2018-09-11 20:50:58 +1200

    Corrected Biuld.sh to remove spaces that somehow added them selves when i copy pasted

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/ bin / bash
-rm *. class # clean project
-javac Calculator . java # compile project
+rm *.class # clean project
+javac Calc.java # compile project
 if [ $? -ne 0 ] # if exit code is not 0 javac failed
 then
 echo " Compile failed !"
 exit 1
 fi
-java -ea Calculator # run test
+java -ea Calc # run test
